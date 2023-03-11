507a36e1ddf2d2cd0e3f3cfd14c076884bf84281
HungNguyen100120hcmunre <118679622+HungNguyen100120hcmunre@users.noreply.github.com>
2023-03-12 01:18:39 +0700

    Add files via upload

---
+++ danhmuc.php
@@ -0,0 +1,33 @@
+<h3>DANH MỤC SẢN PHẨM MỚI NHẤT</h3>
+<ul class="product_list">
+            <li>
+                <a href="">
+                    <img src="images/samsung-galaxy.jpg" />
+                    <p class="title_product">Điện thoại </p>
+                </a>
+            </li>
+            <li>
+                <img src="images/macbook-pro.jpg" />
+                <p class="title_product">Laptop</p>
+            </li>
+            <li>
+                <img src="images/vi-vn-ipad.jpeg" />
+                <p class="title_product">Ipad</p>
+            </li>
+            <li>
+                <img src="images/vi.jpg" />
+                <p class="title_product">Smartwatch</p>
+            </li>
+            <li>
+                <img src="images/titan.jpg" />
+                <p class="title_product">Đồng hồ</p>
+            </li>
+            <li>
+                <img src="images/loa.jpeg" />
+                <p class="title_product">Loa</p>
+            </li>
+            <li>
+                <img src="images/airpods.jpg" />
+                <p class="title_product">Tai nghe</p>
+            </li>
+        </ul>
