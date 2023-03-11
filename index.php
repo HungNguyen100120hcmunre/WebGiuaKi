75dc9cbfb0cc236f24184350f53fe542b6628730
HungNguyen100120hcmunre <118679622+HungNguyen100120hcmunre@users.noreply.github.com>
2023-03-12 01:18:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <title>Thế giới di động</title>
+
+</head>
+
+<body>
+    <div class="wrapper">
+       <div class="header">
+        <img src="images/banner.jpg" width="100%">
+       </div>
+            <div class="menu">
+                <ul class="list_menu">
+                    <li><a href="index.php">Trang chủ</a></li>
+                    <li><a href="danhmuc.php">Danh mục sản phẩm</a></li>
+                    <li><a href="#">Giỏ hàng</a></li>
+                    <li><a href="#">Tin tức</a></li>
+                    <li><a href="#">Liên hệ</a></li>
+                </ul>
+        </div>
+        <div id="main">
+
+            <div class="sidebar">
+                <ul class="list_sidebar">
+                    <li><a href="#"></a>Điện thoại</li>
+                    <li><a href="#">Laptop</a></li>
+                    <li><a href="#">Ipad</a></li>
+                    <li><a href="#">Smartwatch</a></li>
+                    <li><a href="#">Đồng hồ</a></li>
+                    <li><a href="#">Loa</a></li>
+                    <li><a href="#">Tai nghe</a></li>
+                </ul>
+            </div>
+
+            <div class="maincontent">
+            <ul class="product_list">
+                    <li>
+                        <img src="images/iphone.jpg"
+                        <p class="title_product">Iphone 14 Pro Max 128GB</p>
+                        <p class="price_product">29.460.000đ</p>
+                    </li>
+                    <li>
+                        <img src="images/macbook-pro.jpg">
+                        <p class="title_product">Macbook Air 2022</p>
+                        <p class="price_product">29.460.000đ</p>
+                    </li>
+                    <li>
+                        <img src="images/samsung-galaxy.jpg">
+                        <p class="title_product">Samsung galaxy J10</p>
+                        <p class="price_product">22.460.000đ</p>
+                    </li>
+                    <li>
+                        <img src="images/airpods.jpg"
+                        <p class="title_product">Airpods Pro</p>
+                        <p class="price_product">9.460.000đ</p>
+                    </li>
+                    <li>
+                        <img src="images/ipad.jpeg"
+                        <p class="title_product">Ipad Pro M1</p>
+                        <p class="price_product">44.460.000đ</p>
+                    </li>
+                    <li>
+                        <img src="images/titan.jpg"
+                        <p class="title_product">Titan Champagne</p>
+                        <p class="price_product">2.460.000đ</p>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+        <div class="clear">
+
+        </div>
+        <div class="footer">
+            <p>footer</p>
+        </div>
+    </div>
+</body>
+
+</html>
